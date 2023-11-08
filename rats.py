748f24f0cf4ce93dfff35dabb426e4b98671fd1a
rfbaylon <rfbaylon@gmail.com>
2023-11-08 16:55:10 -0500

    read

--- rats.py
+++ rats.py
@@ -7,8 +7,29 @@ import numpy as np
 import matplotlib.pyplot as plt 
 import seaborn as sns
 
+
+'''
+Todo List
+==========
+[] Read in data
+[] Clean data
+[] Create buffer area for proxies
+[] Count 311s within buffers
+[] Statistical Analysis
+[] Make vis mapping all proxies and 311s per year
+[] Make reg prlot for each  proxy per year
+[] clean up the code
+'''
+
+RYAN = "C://Desktop//Code//ds2500//RatCode//Rat-Code//Sources"
+
 def read_data():
+    with open(RYAN, 'w') as f:
+        x = f.write('2023 Rat Calls.csv')
+        print(x)
     return
+
+
 def main():
     return
 
