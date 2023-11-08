42aff50588cbce7275734617448e6a0b6369a4b9
rfbaylon <rfbaylon@gmail.com>
2023-11-08 16:31:17 -0500

    adding data

--- rats.py
+++ rats.py
@@ -1,1 +1,16 @@
-print("Rats")
+# formatting
+from pandasgui import show
+import pandas as pd
+import numpy as np
+
+# visualizers
+import matplotlib.pyplot as plt 
+import seaborn as sns
+
+def read_data():
+    return
+def main():
+    return
+
+if __name__ == "__main__":
+    main()
